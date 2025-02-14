2816232c0e2ce8e5a2cb8eac6b50fcb862156139
pop-ecx <94111959+pop-ecx@users.noreply.github.com>
2025-02-14 15:37:56 +0300

    Update bspwm-dual.sh for duplicate strings

--- bspwm/dual-monitor-bspwm/bspwm-dual.sh
+++ bspwm/dual-monitor-bspwm/bspwm-dual.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+
 if [[ $my_laptop_external_monitor = *connected* ]]; then
-    xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --rotate normal --right-of eDP-1
+    xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal \
+           --output HDMI-1 --mode 1920x1080 --same-as eDP-1
 fi
